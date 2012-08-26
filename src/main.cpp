3bcc9901264a0bc003de2b7773f00560e8989a30
J-P Nurmi <jpnurmi@gmail.com>
2012-08-26 22:35:00 +0200

    Cleanup main()

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,41 @@
-#include <QtGui/QApplication>
+#include <QtDeclarative>
 #include "qmlapplicationviewer.h"
 #include "evernotesession.h"
-#include "db/databasemanager.h"
 #include "cache.h"
 #include "wrappers/tagwrapper.h"
+#include "wrappers/notewrapper.h"
 #include "wrappers/resourcewrapper.h"
 #include "wrappers/notebookwrapper.h"
 
-#include <QtDeclarative>
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
+    QApplication::setApplicationName("EverN9");
     QScopedPointer<QApplication> app(createApplication(argc, argv));
-    QScopedPointer<QmlApplicationViewer> viewer(QmlApplicationViewer::create());
-
-    QString locale = QLocale::system().name();
-
-    QTranslator translator;
-    if (translator.load("EverN9." + locale, ":/")){
-        app.data()->installTranslator(&translator);
-    }else{
-        translator.load("EverN9.en", ":/");
-        app.data()->installTranslator(&translator);
-
-    }
-
-
-    qmlRegisterType <TagWrapper> ("com.evernote.types",1,0, "Tag");
-    qmlRegisterType <NoteWrapper> ("com.evernote.types",1,0, "Note");
-    qmlRegisterType <ResourceWrapper> ("com.evernote.types",1,0, "Resource");
-    qmlRegisterType <NotebookWrapper> ("com.evernote.types",1,0, "Notebook");
 
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 
-    viewer->rootContext()->setContextProperty("EvernoteSession",EvernoteSession::instance());
-    viewer->rootContext()->setContextProperty("DatabaseManager",DatabaseManager::instance());
-    viewer->rootContext()->setContextProperty("Cache",Cache::instance());
+    QTranslator translator;
+    if (translator.load("EverN9." + QLocale::system().name(), ":/"))
+        app->installTranslator(&translator);
+    else if (translator.load("EverN9.en", ":/"))
+        app->installTranslator(&translator);
 
-    viewer->setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
-    if(Settings::instance()->areCredentialsSaved()){
-        viewer->setMainQmlFile(QLatin1String("qml/EverN9/main.qml"));
+    qmlRegisterType<TagWrapper>("com.evernote.types",1,0, "Tag");
+    qmlRegisterType<NoteWrapper>("com.evernote.types",1,0, "Note");
+    qmlRegisterType<ResourceWrapper>("com.evernote.types",1,0, "Resource");
+    qmlRegisterType<NotebookWrapper>("com.evernote.types",1,0, "Notebook");
+
+    QmlApplicationViewer viewer;
+    viewer.rootContext()->setContextProperty("EvernoteSession", EvernoteSession::instance());
+    viewer.rootContext()->setContextProperty("Cache", Cache::instance());
+
+    viewer.setOrientation(QmlApplicationViewer::ScreenOrientationAuto);
+    if (!Settings::value(Settings::Username).isEmpty() && !Settings::value(Settings::Password).isEmpty()) {
+        viewer.setMainQmlFile(QLatin1String("qml/EverN9/main.qml"));
         EvernoteSession::instance()->syncAsync();
-    }else{
-        viewer->setMainQmlFile(QLatin1String("qml/EverN9/mainLogin.qml"));
+    } else {
+        viewer.setMainQmlFile(QLatin1String("qml/EverN9/mainLogin.qml"));
     }
-
-    QObject::connect(viewer->engine(), SIGNAL(quit()), viewer.data(), SLOT(close()));
-    viewer->showExpanded();
-
-
-
+    viewer.showExpanded();
     return app->exec();
 }

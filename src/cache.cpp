e70cdd53f55f12c7eb005686fbbed9234b41be79
J-P Nurmi <jpnurmi@gmail.com>
2012-08-26 19:08:39 +0200

    Cache: minor cleanup

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,76 +1,82 @@
 #include "cache.h"
-#include <QFileDialog>
-#include <QDebug>
-
-#include <QtCore>
-Cache* Cache::m_instance = NULL;
+#include "db/database.h"
+#include "fileutils.h"
 
 Cache::Cache(QObject *parent) :
     QObject(parent)
 {
 }
-Cache* Cache::instance(){
-   if(!m_instance){
-        m_instance = new Cache();
-   }
-   return m_instance;
-}
-Cache::~Cache() {
 
+Cache* Cache::instance()
+{
+    static Cache cache;
+    return &cache;
 }
 
-void Cache::clear(){
+Cache::~Cache()
+{
+}
+
+void Cache::clear()
+{
     tags.clear();
     notebooks.clear();
     notes.clear();
-
 }
-void Cache::load(){
-    tags = DatabaseManager::instance()->getTags();
-    notebooks = DatabaseManager::instance()->getNotebooks();
-    notes = DatabaseManager::instance()->getNotes();
+
+void Cache::load()
+{
+    tags = Database::getTags();
+    notebooks = Database::getNotebooks();
+    notes = Database::getNotes();
     clearNotes();
-    qDebug() << "Notes size: " << notes.size();
     for(int i=0;i<notes.size();i++){
         Note note = notes.at(i);
         NoteWrapper* noteWrapper = new NoteWrapper(note);
         noteAdded(noteWrapper);
     }
 }
-NoteWrapper* Cache::getNote(int index){
+
+NoteWrapper* Cache::getNote(int index)
+{
     return new NoteWrapper(notes.at(index));
 }
-QString Cache::getNoteContent(NoteWrapper* note){
+
+QString Cache::getNoteContent(NoteWrapper* note)
+{
     return FileUtils::readNoteContent(note);
 }
 
-void Cache::clearFileCache(){
+void Cache::clearFileCache()
+{
     FileUtils::removeNoteDir();
 }
-void Cache::openTestFileWindow(){
-    QFileDialog * fd = new QFileDialog(NULL);
-    fd->setAcceptMode(QFileDialog::AcceptOpen);
-    fd->setFileMode(QFileDialog::ExistingFiles);
-    fd->exec();
-}
-void Cache::fillWithTags(){
+
+void Cache::fillWithTags()
+{
     for(int i=0;i<tags.size();i++){
         Tag tag = tags.at(i);
         TagWrapper* wrapper = new TagWrapper(tag);
         tagFired(wrapper);
     }
 }
-void Cache::fillWithNotebooks(){
+
+void Cache::fillWithNotebooks()
+{
     for(int i=0;i<notebooks.size();i++){
         Notebook notebook = notebooks.at(i);
         NotebookWrapper* wrapper = new NotebookWrapper(notebook);
         notebookFired(wrapper);
     }
 }
-QString Cache::getCacheFileName(NoteWrapper* note){
+
+QString Cache::getCacheFileName(NoteWrapper* note)
+{
     return FileUtils::noteContentFilePath(note);
 }
-NotebookWrapper* Cache::getNotebook(NoteWrapper* note){
+
+NotebookWrapper* Cache::getNotebook(NoteWrapper* note)
+{
     for(int i=0;i<notebooks.size();i++){
         Notebook notebook = notebooks.at(i);
         if(notebook.guid.compare(note->getNotebookGUID().toStdString()) == 0){
@@ -84,7 +90,7 @@ NotebookWrapper* Cache::getNotebook(NoteWrapper* note){
        return;
     }
     clearTags();
-    tags = DatabaseManager::instance()->getTags();
+    tags = Database::getTags();
     for(int i=0;i<tags.size();i++){
         Tag tag = tags.at(i);
         TagWrapper* tagWrapper = new TagWrapper(tag);
--- src/cache.h
+++ src/cache.h
@@ -1,29 +1,20 @@
 #ifndef CACHE_H
 #define CACHE_H
-#include "edam/UserStore.h"
-#include "edam/UserStore_constants.h"
-#include "edam/NoteStore.h"
-#include "edam/NoteStore_constants.h"
-#include "edam/NoteStore_types.h"
+
+#include "edam/Types_types.h"
 #include "wrappers/tagwrapper.h"
 #include "wrappers/notewrapper.h"
 #include "wrappers/notebookwrapper.h"
-#include "db/databaseconstants.h"
-#include "db/databasemanager.h"
-#include "settings.h"
-#include "evernotesession.h"
-#include <QtCore>
 #include <QObject>
-#include <QStringListModel>
-using namespace evernote::edam;
-using namespace std;
+#include <QVector>
 
 class Cache : public QObject
 {
     Q_OBJECT
+
 public:
     static Cache* instance();
-    Cache(QObject *parent = 0);
+    Cache(QObject* parent = 0);
     ~Cache();
 
 signals:
@@ -31,22 +22,22 @@ signals:
     void noteAdded(NoteWrapper* note);
     void tagFired(TagWrapper* tag);
     void notebookFired(NotebookWrapper* notebook);
+
 public slots:
     void load();
     void clear();
     NoteWrapper* getNote(int index);
     void clearFileCache();
-    void openTestFileWindow();
     void fillWithTags();
     void fillWithNotebooks();
     QString getNoteContent(NoteWrapper* note);
     QString getCacheFileName(NoteWrapper* note);
     NotebookWrapper* getNotebook(NoteWrapper* note);
+
 private:
-    static Cache* m_instance;
-    QVector<Tag> tags;
-    QVector<Notebook> notebooks;
-    QVector<Note> notes;
+    QVector<evernote::edam::Notebook> notebooks;
+    QVector<evernote::edam::Note> notes;
+    QVector<evernote::edam::Tag> tags;
 };
 
 #endif // CACHE_H

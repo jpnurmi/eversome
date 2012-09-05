c0c4ae6697929ad09dfae6552fd0de1855e63472
J-P Nurmi <jpnurmi@gmail.com>
2012-09-05 10:13:11 +0200

    Add -reset command line argument

--- src/EverN9/main.cpp
+++ src/EverN9/main.cpp
@@ -13,6 +13,24 @@
 #include "noteitem.h"
 #include "tagitem.h"
 
+static bool removeDir(const QDir& dir)
+{
+    bool result = true;
+    if (dir.exists()) {
+        QFileInfoList entries = dir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files);
+        foreach (const QFileInfo &info, entries) {
+            if (info.isDir())
+                result &= removeDir(QDir(info.absoluteFilePath()));
+            else
+                result &= QFile::remove(info.absoluteFilePath());
+        }
+        QDir parent(dir);
+        if (parent.cdUp())
+            parent.rmdir(dir.dirName());
+    }
+    return result;
+}
+
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QApplication::setApplicationName("EverN9");
@@ -20,6 +38,21 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+
+    QString dataPath = QDesktopServices::storageLocation(QDesktopServices::DataLocation);
+    QString cachePath = QDesktopServices::storageLocation(QDesktopServices::CacheLocation);
+    QString configPath = QFileInfo(QSettings().fileName()).absolutePath();
+
+    if (app->arguments().contains("-reset")) {
+        qDebug() << "EverN9 reset...";
+        qDebug() << "  -> Data:" << (removeDir(dataPath) ? "OK" : "FAIL!") << qPrintable("("+dataPath+")");
+        qDebug() << "  -> Cache:" << (removeDir(cachePath) ? "OK" : "FAIL!") << qPrintable("("+cachePath+")");
+        qDebug() << "  -> Config:" << (removeDir(configPath) ? "OK" : "FAIL!") << qPrintable("("+configPath+")");
+    }
+
+    QDir().mkpath(configPath);
+    QDir().mkpath(cachePath);
+    QDir().mkpath(dataPath);
 
     qmlRegisterType<TagItem>("com.evernote.types", 1,0, "Tag");
     qmlRegisterType<NoteItem>("com.evernote.types", 1,0, "Note");

8420563c888517b2baaae0084c075ee80e83bf22
J-P Nurmi <jpnurmi@gmail.com>
2012-09-10 22:53:28 +0200

    Restore QApplication. MComponentData seems enough for AccountsUI

--- src/EverN9/main.cpp
+++ src/EverN9/main.cpp
@@ -12,7 +12,7 @@
 * GNU General Public License for more details.
 */
 #include <QtDeclarative>
-#include <MApplication>
+#include <MComponentData>
 #include "qmlapplicationviewer.h"
 
 #include "filesystem.h"
@@ -34,7 +34,13 @@ Q_DECL_EXPORT int main(int argc, char* argv[])
 {
     QApplication::setApplicationName("EverN9");
     QApplication::setOrganizationName("Evernote");
-    MApplication app(argc, argv);
+    QScopedPointer<QApplication> app(createApplication(argc, argv));
+
+    // MComponentData instance required by AccountsUI
+    MComponentData::createInstance(argc, argv);
+    // thanks, but no thanks, MComponentData...
+    // -> restore the default Qt msg handler
+    qInstallMsgHandler(0);
 
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 
@@ -42,7 +48,7 @@ Q_DECL_EXPORT int main(int argc, char* argv[])
     QString cachePath = QDesktopServices::storageLocation(QDesktopServices::CacheLocation);
     QString configPath = QFileInfo(QSettings().fileName()).absolutePath();
 
-    QStringList args = app.arguments();
+    QStringList args = app->arguments();
     if (args.contains("-reset")) {
         qDebug() << "EverN9 reset...";
         qDebug() << "  -> Data:" << (FileSystem::removeDir(dataPath) ? "OK" : "FAIL!") << qPrintable("("+dataPath+")");
@@ -83,12 +89,12 @@ Q_DECL_EXPORT int main(int argc, char* argv[])
         session.establish(account.credentialsId());
         viewer.showExpanded();
     } else {
-        QObject::connect(&account, SIGNAL(failed()), &app, SLOT(quit())); // TODO
-        QObject::connect(&account, SIGNAL(cancelled()), &app, SLOT(quit()));
+        QObject::connect(&account, SIGNAL(failed()), app.data(), SLOT(quit())); // TODO
+        QObject::connect(&account, SIGNAL(cancelled()), app.data(), SLOT(quit()));
         QObject::connect(&account, SIGNAL(created(int)), &session, SLOT(establish(int)));
         QObject::connect(&account, SIGNAL(created(int)), &viewer, SLOT(showFullScreen()));
         QTimer::singleShot(0, &account, SLOT(create()));
     }
 
-    return app.exec();
+    return app->exec();
 }
